0a40f5844d63bfadec66f0225b7879ab1fdd73d0
shirayama-mai <138377007+shirayama-mai@users.noreply.github.com>
2023-10-31 21:12:59 +0900

    extended interfaces

--- src/@types/foriio.d.ts
+++ src/@types/foriio.d.ts
@@ -8,26 +8,7 @@ declare namespace Foriio {
             "total_pages": number,
             "total_count": number
         },
-        "works": {
-            "id": number,
-            "title": string,
-            "thumbnail": "string",
-            "thumbnail_id": number,
-            "notes_count": number,
-            "decription": string,
-            /** Stringified pbliced date.
-             * 
-             * 
-             * Format example)
-             * 
-             * "2022-11-11T11:39:22.342Z"
-             */
-            "published_at": string,
-            "is_nsfw": boolean,
-            "author": User,
-            "type": "image" | "",
-            "author_id": number
-        }[]
+        "works": Foriio.data.DefaultWork[]
     };
 
     /** Information about the user associated with the API Key */
@@ -60,6 +41,8 @@ declare namespace Foriio {
         };
     }
 
+    
+
     /** Error thrown when authentication fails */
     export interface AuthenticationError {        
         "status": 401,
@@ -75,54 +58,74 @@ declare namespace Foriio {
     export type RequestWorks = (token: string) => Promise<Foriio.Works | Foriio.AuthenticationError>;
 
     export type RequestUser = (token: string) => Promise<User | AuthenticationError>;
+};
+
+declare namespace Foriio.data {
+    export interface Image {
+        "id": number,
+        "urls": {
+            "list": string,
+            "list2x": string,
+            "detail": string,
+            "detail2x": string,
+            "profile_header_image": strin | null;
+        },
+        "srcset": string,
+        "width": number,
+        "height": number
+    };
+
+    export interface Video {
+        "platform": "youtube" | "",
+        "video_id": string,
+        "url": string
+        "title": string,
+        "description": string,
+        "picture_url": string
+    }
+
+    export interface WebArticle {
+        "url": string,
+        "title": string,
+        "description": string,
+        "og_type": "article", "website",
+        "domain": string,
+        "image": string
+    };
+
+    export interface DefaultWork {
+        "id": number,
+        "title": string,
+        "thumbnail": string,
+        "thumbnail_id": number | null,
+        "notes_count": number,
+        "decription": string,
+        /** Stringified pbliced date.
+         * 
+         * 
+         * Format example)
+         * 
+         * "2022-11-11T11:39:22.342Z"
+         */
+        "published_at": string,
+        "is_nsfw": boolean,
+        "author": User,
+        "type": "copy_writing" | "image" | "video" | "web_article",
+        "author_id": number
+    }
+
+    export interface ImageWork extends DefaultWork {
+        "type": "image"
+        "images": Image[]
+    }
+
+    export interface VideoWork extends DefaultWork {
+        "type": "video",
+        "videos": Video[]
+    }
+
+    export interface WebArticleWork extends DefaultWork {
+        "type": "web_article",
+        "web_article": WebArticle[]
+    }
 }
-
-// declare namespace Foriio {
-//     export type Works = {
-//         "meta": {
-//             "current_page": 1,
-//             "per_page": 50,
-//             "total_pages": 1,
-//             "total_count": 1
-//         },
-//         "works": {
-//             "id": 10,
-//             "title": "image 13",
-//             "thumbnail": "string",
-//             "thumbnail_id": 11,
-//             "notes_count": 0,
-//             "decription": "sample work",
-//             "published_at": "2022-11-11T11:39:22.342Z",
-//             "is_nsfw": false,
-//             "author": {
-//                 "id": 2,
-//                 "screen_name": "john",
-//                 "profile": User,
-//                 "status": "approved",
-//                 "is_pro": true
-//             },
-//             "type": "image",
-//             "author_id": 1543
-//         }[]
-//     };
-
-//      export type User = {
-//          'user': {
-//              "id": 2,
-//              "screen_name": "john",
-//              "profile": {
-//                  "id": 223,
-//                  "name": "sample author",
-//                  "profession": "UI",
-//                  "twitter_url": "https://twitter.com/",
-//                  "avatar": {
-//                      "thumb2x": "https://d1yy3fmo6s8c2k.cloudfront.net/store/b79bd73a723d9c0d986ba6294ead799b.jpg",
-//                      "thumb": "https://d1yy3fmo6s8c2k.cloudfront.net/store/7b1be39516759d5eb82d304b9c59d8a5.jpg",
-//                      "phone": "https://d1yy3fmo6s8c2k.cloudfront.net/store/1e69dd7c19d2b72f7fd82188f802c373.jpg"
-//                  }
-//              },
-//              "status": "approved",
-//              "is_pro": true
-//          };
-//      }
-// }
